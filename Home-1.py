2d64ac186a0ef86ac6388b330a55a03b9db3fac9
Prakit Junkhum <prakit340@hotmail.com>
2023-09-17 09:52:32 +0700

    Update Home-1.py

--- Home-1.py
+++ Home-1.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
 
-st.header('kairung')
+st.header('PRAKIT JUNKHUM')
 #st.image("./pic/kairung.jpg")
 col1, col2, col3 = st.columns(3)
 
